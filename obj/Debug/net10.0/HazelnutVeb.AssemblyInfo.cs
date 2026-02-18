7f6934d17f869b6ba84044304a3c0ed269dd83bf
Daredare123 <stojanceskidario12@gmail.com>
2026-02-18 12:41:49 +0100

    Added mobile app PWA

--- obj/Debug/net10.0/HazelnutVeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/HazelnutVeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HazelnutVeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da1d61d699271fe0816f688d8f704553a3d69a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3151f09c2f9f12d18ba5505c74603fc872b97f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HazelnutVeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HazelnutVeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
